5bfd8958fb37ceb60da8d6faeec9963c7eb6fd1a
dory-finn <dorystudio.lab@gmail.com>
2026-06-03 01:35:42 +0800

    Upgrade Next to 16.2.7

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,31 +1,30 @@
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-import { FlatCompat } from "@eslint/eslintrc"
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
+import unusedImports from 'eslint-plugin-unused-imports';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
-    rules: {
-      "@next/next/no-duplicate-head": "off",
-      "unused-imports/no-unused-imports": 1,
-      "unused-imports/no-unused-vars": [
-        1,
-        {
-          vars: "all",
-          varsIgnorePattern: "^_",
-          args: "after-used",
-          argsIgnorePattern: "^_",
+const eslintConfig = defineConfig([
+    ...nextVitals,
+    ...nextTypescript,
+    globalIgnores(['dist-scripts/**', 'release/**', '.tmp/**', 'localdata/**', 'public/monaco-editor/**']),
+    {
+        plugins: {
+            'unused-imports': unusedImports,
         },
-      ],
+        rules: {
+            '@next/next/no-duplicate-head': 'off',
+            'unused-imports/no-unused-imports': 1,
+            'unused-imports/no-unused-vars': [
+                1,
+                {
+                    vars: 'all',
+                    varsIgnorePattern: '^_',
+                    args: 'after-used',
+                    argsIgnorePattern: '^_',
+                },
+            ],
+        },
     },
-  }),
-]
+]);
 
-export default eslintConfig
+export default eslintConfig;
