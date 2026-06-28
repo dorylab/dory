21180463568842b4923b029bf4331deebe4fd330
dory-finn <dorystudio.lab@gmail.com>
2026-06-28 16:40:34 +0800

    Enable Better Auth joins

--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -217,9 +217,9 @@ function createAuth() {
 
         const auth = betterAuth({
             appName: 'Dory',
-            // experimental: {
-            //     joins: true,
-            // },
+            experimental: {
+                joins: true,
+            },
             database: drizzleAdapter(db, { provider, schema }),
             plugins: [
                 ...authPlugins,
